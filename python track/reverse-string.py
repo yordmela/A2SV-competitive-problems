bbfb10538b7bf4aa016e7f3954b248b069f28e41
yordmela <119792940+yordmela@users.noreply.github.com>
2023-12-22 18:47:42 +0300

    Added solution for Reverse String

---
+++ python track/reverse-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseString(self, s):
+        l,r=0, len(s)-1
+        while l<r:
+            s[l],s[r]= s[r],s[l]
+            l+=1
+            r-=1
+        return s
+
+
+
+        
