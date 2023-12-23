655661a9f120ed1468039059b5164b7b06246e23
yordmela <119792940+yordmela@users.noreply.github.com>
2023-12-23 21:47:35 +0300

    Added solution for Transpose Matrix

---
+++ python track/transpose-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        newMat=[[0]*len(matrix) for _ in range(len(matrix[0]))] 
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                newMat[j][i]= matrix[i][j]
+        return newMat
+        
