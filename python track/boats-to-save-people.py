ed961d2d9cc3af1d093f7c3dd4875373fcbd47bb
yordmela <119792940+yordmela@users.noreply.github.com>
2024-01-03 19:28:11 +0300

    Added solution for Boats to Save People

---
+++ python track/boats-to-save-people.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+       people.sort()
+       left, right= 0, len(people)-1
+       boats=0
+       while left<=right:
+          if people[left]+people[right]<= limit:
+              left+=1
+              right-=1
+          else:
+              right-=1
+          boats+=1
+       return boats
+           
+
+        
