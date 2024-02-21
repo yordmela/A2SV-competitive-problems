dca6101d7a459458345e2808e5c7af3450f0799f
yordmela <119792940+yordmela@users.noreply.github.com>
2024-02-21 09:46:37 +0300

    Add solution for Merge Two Sorted Lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -5,29 +5,15 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode(-1)
-        curr1= list1
-        curr2= list2
-        if (curr1 and curr2 and curr1.val< curr2.val) or (not curr2 and curr1):
-            dummy.next= curr1
-            curr1= curr1.next
-        elif (curr1 and curr2) or(not curr1 and curr2):
-            dummy.next= curr2
-            curr2= curr2.next
-        curr= dummy.next
-        
-        while curr1 and curr2 and curr:
-            if curr1 and curr2 and curr1.val< curr2.val:
-                curr.next= curr1
-                curr1= curr1.next
-            else:
-                curr.next= curr2
-                curr2= curr2.next
-            curr= curr.next
-        if curr1 and curr:
-            curr.next= curr1
-        if curr2 and  curr:
-            curr.next= curr2
-        return dummy.next
-
-    
+        #base cases
+        if list1==None:
+            return list2
+        if list2==None:
+            return list1
+        # recurssive relations
+        if list1.val<list2.val:
+            list1.next= self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next= self.mergeTwoLists(list1, list2.next)
+            return list2
